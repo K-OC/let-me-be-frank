cbec7194096eb0c4ee3ef142bb0a612d4e0bbfff
Kieran O'Connell <kieranpoconnell@gmail.com>
2021-06-19 18:09:37 -0230

    api and listing grid

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,18 @@ import styled from "styled-components";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Header from "./components/Header";
 import GlobalStyles from "./components/GlobalStyles";
+import { ListingGrid } from "./components/ListingGrid";
 function App() {
   return (
     <>
       <BrowserRouter>
         <GlobalStyles />
         <Header />
+        <Switch>
+          <Route path="/" exact>
+            <ListingGrid />
+          </Route>
+        </Switch>
       </BrowserRouter>
     </>
   );
--- client/src/components/ListingGrid.js
+++ client/src/components/ListingGrid.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
+require("dotenv").config();
 
 export const ListingGrid = () => {
+  console.log(`${process.env.REACT_APP_API_KEY}`);
   return <div></div>;
 };
