103fbdc33265a68192eb6d11ee2bc1c3ff899492
Kieran O'Connell <kieranpoconnell@gmail.com>
2021-06-24 14:41:22 -0230

    footer and about page

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,1 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+export const Footer = () => {
+    return (
+        <>
+        <Wrapper>
+           <StyledLink>About</StyledLink>
+        </Wrapper>
+        </>
+    )
+}
+
+const Wrapper = styled.div`
+font-size: 100px;
+`
+const StyledLink = styled.a``
+
