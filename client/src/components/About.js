3e2b51a129b6c851a455f214a6a99a515d1904e9
Kieran O'Connell <kieranpoconnell@gmail.com>
2021-06-26 18:27:37 -0230

    about

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,16 +1,26 @@
-import React from "react"
-import styled from "styled-components"
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export const About = () => {
-    return (
-        <>
-        <Wrapper>
-            Hello
-        </Wrapper>
-        </>
-    )
-}
+  return (
+    <>
+      <Wrapper>
+        <StyledTitle>Why would you do this?</StyledTitle>
+      </Wrapper>
+    </>
+  );
+};
 
 const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
 
-`
+const StyledTitle = styled.h1`
+  font-family: var(--header-font);
+  color: black;
+  margin-top: 50%;
+`;
