18f4539ac1c6999d2e095cbb9a45d4276873146d
Kieran O'Connell <kieranpoconnell@gmail.com>
2021-06-27 12:58:43 -0230

    copy added

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -7,6 +7,21 @@ export const About = () => {
     <>
       <Wrapper>
         <StyledTitle>Why would you do this?</StyledTitle>
+        <StyledPee>
+          Let Me Be Frank is a product of the developer's fandom of the{" "}
+          <StyledRedirect to="https://headgum.com/doughboys">
+            doughboys podcast
+          </StyledRedirect>
+          . On the show, there is a recurring segment called Let Me Be Frank.
+          The segment kicks off with host Nick "Frank" Wiger aka The Burger Boy
+          singing an increasingly elaborate theme song. Following this, host
+          Mike "Spoon Man" Mitchell aka Mitch aka Mr. Slice googles "hot dog" or
+          "hot dogs", or "hut dogs", and then feeds Nick and the guest some hot
+          dog news. This project uses NewsOrg Api to scrape news about hot dogs.
+          The search terms used are "hot-dog", "hotdogs", "hot-dogs", "sausage"
+          and "hotdog". Search terms are specifically found in the title of the
+          article.
+        </StyledPee>
       </Wrapper>
     </>
   );
@@ -24,3 +39,5 @@ const StyledTitle = styled.h1`
   color: black;
   margin-top: 50%;
 `;
+const StyledPee = styled.p``;
+const StyledRedirect = styled.a``;
